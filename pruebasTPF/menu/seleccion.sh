4acfb0fdcf6d967380a02a0dc41d9334d18e1faa
JMZanni <juanmzanni@gmail.com>
2023-07-18 19:27:58 -0300

    Se completa la prueba de menu

--- pruebasTPF/menu/seleccion.sh
+++ pruebasTPF/menu/seleccion.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
+PS3='Seleccione>'
+echo "Elija una opción"
+select opcion in "Salir" "Hola" "Insulto" "Chau"
+do
+#	[ -e $opcion ] && echo "Elegir opcion valida!" && continue
+	[ $REPLY == 1 ] && echo "Me fui" && break
+	[ $REPLY == 2 ] && bash "hola.sh" && continue
+        [ $REPLY == 3 ] && bash "insulto.sh" && continue
+        [ $REPLY == 4 ] && bash "chau.sh" && continue
+	echo "Opción elegida: " $opcion
+done
+
 exit 0
